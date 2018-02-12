a0310c44e777773a46a6dcbd0a916e4c6fccfa58
dsulli <dtcsulli@gmail.com>
2018-02-11 22:15:36 -0800

    a small test

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://dsulli.github.io/art/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://dsulli.github.io/art/',
 
     /**
      * Source Maps
