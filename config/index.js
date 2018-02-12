612cc9ae4de3de65dbc75e6bc5b2bef1788286a3
dsulli <dtcsulli@gmail.com>
2018-02-11 21:53:02 -0800

    vcg test

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://dsulli.github.io/art/',
     proxyTable: {},
 
     // Various Dev Server settings
