460717396aa5a376144fb869b5d52da00b2be0fd
deanna <dtcsulli@gmail.com>
2019-05-14 23:12:17 -0700

    Updates

--- sw.js
+++ sw.js
@@ -6,16 +6,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "917629a3b768e62a4c782c7dced3a1db"
   },
   {
-    "url": "/_nuxt/14152fc1dc9b4d236fb8.js",
-    "revision": "4431d4fe66d627bed476478b3ac3a7f5"
+    "url": "/_nuxt/133a189cdb485b005290.js",
+    "revision": "73451dac5a9b6052601e37adbbaaa3a4"
   },
   {
     "url": "/_nuxt/3669c5915038e7ac5a63.js",
     "revision": "1e13355fb7215c174ca7a3987d3b3350"
   },
   {
-    "url": "/_nuxt/8f53cbfeb9ddd223797e.js",
-    "revision": "4d52aa7698df85aaba8bfa647a87725e"
+    "url": "/_nuxt/382b2b110d0114effd9b.js",
+    "revision": "38a70969a152c5f94cef275549024a68"
   }
 ], {
   "cacheId": "art",
